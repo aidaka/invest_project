3eb85d3367038d1dd95136676045f8a1d6ba2164
mrcrysmile <275076698@qq.com>
2019-12-10 19:22:49 +0800

    1

--- data_mining_homework.py
+++ data_mining_homework.py
@@ -9,32 +9,37 @@ import pandas as pd
 def classify(data, class_num, max_iter, stop_loss):
     # 保存结果
     res_list = []
-    # 保存每次的中心点及序号
+    # 保存每次的中心点
     center_list = []
-    center_index_list = []
+    # 保存每类所有点的和，用于计算下一次的中心点
+    sum_point_list = []
     # 判断数据量是否符合聚类要求
     if data.shape[0] <= class_num:
         print("Are You Kidding Me?")
         return
     
-    # 初始化中心点
+    # 初始化
     for i in class_num:
         center_list.append(data[i])
-        center_index_list.append(i)
+        res_list.append(list())
+        sum_point_list.append(list(0.0, 0.0))
 
     # 聚类
     for i in range(max_iter):
         for index in range(len(data)):
-            # 跳过中心点
-            if index in center_index_list:
-                continue
             # 寻找最近中心点
             min_dis = np.sum(np.square(center_list[0] - data[index]))
             temp_dis = 0.0
+            # 点所属的类别
+            _class = 0
             for j in range(1, 5):
                 temp_dis = np.sum(np.square(center_list[j] - data[index]))
-                min_dis = min(min_dis, temp_dis)
-            
+                if temp_dis < min_dis:
+                    min_dis = temp_dis
+                    _class = j
+            res_list[_class].append(index)
+            sum_point_list[_class][0] += data[index][0]
+            sum_point_list[_class][1] += data[index][1]
             
     res_list.append(1)
     return 
